158b1a64b4dd1cf787e4591bf5747081810670d7
Dmytro-Doronin <doronindmytro623@gmail.com>
2026-01-28 21:03:45 +0100

    pagination

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,8 +3,18 @@ import { Features } from '../../components/sections/Features/Features';
 import {PopularSubjects} from "../../components/sections/PopularSubjects/PopularSubjects.tsx";
 
 export const Home = () => {
+    // const indexChange = (page: number) => {
+    //     console.log(page);
+    // }
   return (
     <div className="min-h-screen">
+        {/*<RadioGroup options={*/}
+        {/*    [*/}
+        {/*        {label: "English", value: "english"},*/}
+        {/*        {label: "German", value: "german"}*/}
+        {/*    ]*/}
+        {/*}/>*/}
+        {/*<Pagination theme='primary' shape='round' activeIndex={1} totalPages={20} onIndexChange={indexChange} siblings={2}/>*/}
       <Hero />
       <Features />
       <PopularSubjects />
