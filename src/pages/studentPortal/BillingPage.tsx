03b0575bd766c801b8988031bcaf64a402f1a9e9
Majoodeh <Majdhussein144@gmail.com>
2026-02-06 14:52:11 +0100

    feature(studentPortal): add General, My Lessons, Billing, VideoCall, and Settings Pages

---
+++ src/pages/studentPortal/BillingPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const BillingPage = () => {
+  return <div>BillingPage</div>;
+};
---
+++ src/pages/studentPortal/VideoCallPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const VideoCallPage = () => {
+  return <div>VideoCallPage</div>;
+};
